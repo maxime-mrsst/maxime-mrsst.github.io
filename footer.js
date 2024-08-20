65554e1b717096f31806b2f18499e9801e4408ae
Morisset Maxime <maxime.morisset@u-bordeaux.fr>
2024-08-20 21:50:00 +0200

    Test header.js

---
+++ footer.js
@@ -0,0 +1,6 @@
+let appFooter = `
+    <nav>
+       /*navigation or other html content here*/
+    </nav>
+`;
+document.getElementById("app-footer").innerHTML = appFooter;
---
+++ header.js
@@ -0,0 +1,9 @@
+let appHeader = `
+    <nav>
+        <a href="#">Accueil</a>
+        <a href="#">Publications</a>
+        <a href="#">Enseignement</a>
+        <a href="#">CV</a>
+    </nav>
+`;
+document.getElementById("app-header").innerHTML = appHeader;
